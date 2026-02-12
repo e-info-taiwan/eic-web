c71795693cb04c46718c60f40ca9f7f275ce3eb7
Chaowen Chang <chaowen@cloudmosa.com>
2026-02-12 15:34:30 +0800

    fix: remove unused $firebaseId variable from member-vote GraphQL query

--- packages/e-info/pages/api/poll/member-vote.ts
+++ packages/e-info/pages/api/poll/member-vote.ts
@@ -23,7 +23,7 @@ type PollResult = {
 
 // Query to verify member owns this firebaseId
 const VERIFY_MEMBER_OWNERSHIP = `
-  query VerifyMemberOwnership($memberId: ID!, $firebaseId: String!) {
+  query VerifyMemberOwnership($memberId: ID!) {
     member(where: { id: $memberId }) {
       id
       firebaseId
@@ -86,7 +86,7 @@ export default async function handler(
     // Verify the memberId belongs to this firebaseId
     const ownershipResult = await serverGraphQL<{
       member: { id: string; firebaseId: string } | null
-    }>(VERIFY_MEMBER_OWNERSHIP, { memberId, firebaseId })
+    }>(VERIFY_MEMBER_OWNERSHIP, { memberId })
 
     if (ownershipResult.error) {
       console.error(
