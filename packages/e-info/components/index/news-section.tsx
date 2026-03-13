d0442d7721c1bbac35b77c2b9f8aefe67cb1471b
Chaowen Chang <chaowen@cloudmosa.com>
2026-03-13 14:28:44 +0800

    fix: add line-clamp 3 to FeaturedBrief to match NewsBrief

--- packages/e-info/components/index/news-section.tsx
+++ packages/e-info/components/index/news-section.tsx
@@ -305,6 +305,10 @@ const FeaturedBrief = styled.p`
   font-weight: 400;
   line-height: 1.5;
   margin: 0;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 
   @media (min-width: ${({ theme }) => theme.mediaSize.md}px) {
     font-size: 18px;
