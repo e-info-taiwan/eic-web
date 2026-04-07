6ef29b056030b97777e474a8adf6ec27409083ee
Chaowen Chang <chaowen@cloudmosa.com>
2026-04-07 17:51:35 +0800

    fix: update VIDEO-V2 caption style to match image figcaption design

--- packages/draft-renderer/lib/website/eic/block-renderers/video-v2-block.js
+++ packages/draft-renderer/lib/website/eic/block-renderers/video-v2-block.js
@@ -5,17 +5,38 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.VideoV2Block = VideoV2Block;
 var _react = _interopRequireDefault(require("react"));
+var _styledComponents = _interopRequireDefault(require("styled-components"));
 var _embeddedCodeBlock = require("./embedded-code-block");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var Figure = _styledComponents["default"].figure.withConfig({
+  displayName: "video-v2-block__Figure",
+  componentId: "sc-as4s45-0"
+})(["width:100%;", ";", "{margin:0;}"], function (_ref) {
+  var theme = _ref.theme;
+  return theme.margin["default"];
+}, _embeddedCodeBlock.Block);
+var FigureCaption = _styledComponents["default"].figcaption.withConfig({
+  displayName: "video-v2-block__FigureCaption",
+  componentId: "sc-as4s45-1"
+})(["width:100%;font-size:12px;line-height:1.25;text-align:justify;color:", ";padding:0;margin:8px 0 0;", "{line-height:1.25;}a{color:", ";text-decoration:underline;}"], function (_ref2) {
+  var theme = _ref2.theme;
+  return theme.colors.grayscale[20];
+}, function (_ref3) {
+  var theme = _ref3.theme;
+  return theme.breakpoint.xl;
+}, function (_ref4) {
+  var theme = _ref4.theme;
+  return theme.colors.primary[20];
+});
 function VideoV2Block(entity) {
   var _entity$getData = entity.getData(),
     embedMarkup = _entity$getData.embedMarkup,
     captionHtml = _entity$getData.captionHtml;
-  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement(_embeddedCodeBlock.Block, {
+  return /*#__PURE__*/_react["default"].createElement(Figure, null, /*#__PURE__*/_react["default"].createElement(_embeddedCodeBlock.Block, {
     dangerouslySetInnerHTML: {
       __html: embedMarkup
     }
-  }), captionHtml && /*#__PURE__*/_react["default"].createElement(_embeddedCodeBlock.Caption, {
+  }), captionHtml && /*#__PURE__*/_react["default"].createElement(FigureCaption, {
     dangerouslySetInnerHTML: {
       __html: captionHtml
     }
--- packages/draft-renderer/src/website/eic/block-renderers/video-v2-block.tsx
+++ packages/draft-renderer/src/website/eic/block-renderers/video-v2-block.tsx
@@ -1,20 +1,49 @@
 import React from 'react'
 import { DraftEntityInstance } from 'draft-js'
-import { Block, Caption } from './embedded-code-block'
+import styled from 'styled-components'
+import { Block } from './embedded-code-block'
+
+const Figure = styled.figure`
+  width: 100%;
+  ${({ theme }) => theme.margin.default};
+
+  ${Block} {
+    margin: 0;
+  }
+`
+
+const FigureCaption = styled.figcaption`
+  width: 100%;
+  font-size: 12px;
+  line-height: 1.25;
+  text-align: justify;
+  color: ${({ theme }) => theme.colors.grayscale[20]};
+  padding: 0;
+  margin: 8px 0 0;
+
+  ${({ theme }) => theme.breakpoint.xl} {
+    line-height: 1.25;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primary[20]};
+    text-decoration: underline;
+  }
+`
 
 export function VideoV2Block(entity: DraftEntityInstance) {
   const { embedMarkup, captionHtml } = entity.getData()
 
   return (
-    <div>
+    <Figure>
       <Block
         dangerouslySetInnerHTML={{
           __html: embedMarkup,
         }}
       />
       {captionHtml && (
-        <Caption dangerouslySetInnerHTML={{ __html: captionHtml }} />
+        <FigureCaption dangerouslySetInnerHTML={{ __html: captionHtml }} />
       )}
-    </div>
+    </Figure>
   )
 }
