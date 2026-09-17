952ff2dc005557ddf3e2b3a05d427b40ed1e989f
hcchien <hcchien@gmail.com>
2026-09-17 19:19:50 +0800

    feat: serve sitemap index and declare it in robots

--- packages/e-info/pages/api/robots.ts
+++ packages/e-info/pages/api/robots.ts
@@ -5,8 +5,11 @@ import { ENV } from '~/constants/environment-variables'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Content-Type', 'text/plain')
   if (ENV === 'prod') {
-    res.write(`User-agent: * 
-Allow: / `)
+    res.write(`User-agent: *
+Allow: /
+
+Sitemap: https://e-info.org.tw/sitemap.xml
+`)
   } else {
     res.write(`User-agent: * 
 Disallow: /`)
